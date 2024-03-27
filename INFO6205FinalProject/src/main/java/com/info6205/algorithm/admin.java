5951d24c8e28406dd5ae166f4575d19d813395c6
ZihaoLu1106 <lu.ziha@northeastern.edu>
2024-03-27 17:55:37 -0400

    add ui

--- INFO6205FinalProject/src/main/java/com/info6205/algorithm/admin.java
+++ INFO6205FinalProject/src/main/java/com/info6205/algorithm/admin.java
@@ -1,8 +1,32 @@
 package com.info6205.algorithm;
 
+import java.util.List;
+
 public class admin {
 
     //singleton
+    private static admin instance;
+    public static admin getInstance() {
+        // If instance is null, create a new instance
+        if (instance == null) {
+            instance = new admin();
+        }
+        // Return the instance
+        return instance;
+    }
+    private List<Order>orderList;
+    private List<Order>historyOrders;
+    private TreeToCategorizeCloth cateegorizedTree;
+
+    private QueueForPendingCLothGroup queue;
+
+    private BSTForWorkingSlot bst;
+
+
+    public admin(){
+
+
+    }
 
 
 }
--- INFO6205FinalProject/src/main/java/com/info6205/info6205finalproject/CreateOrderController.java
+++ INFO6205FinalProject/src/main/java/com/info6205/info6205finalproject/CreateOrderController.java
@@ -1,12 +1,40 @@
 package com.info6205.info6205finalproject;
 
+import com.info6205.algorithm.Order;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class CreateOrderController {
+    private Order newOrder;
+
+    @FXML
+    private ComboBox<String>colorBox;
+    @FXML
+    private ComboBox<String>materialBox;
+    @FXML
+    private TableView cartTable;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button checkPutButton;
+    @FXML
+    public void initialize() {
+        newOrder=new Order();
+
+
+        colorBox.getItems().removeAll(colorBox.getItems());
+        colorBox.getItems().addAll("White", "Deep", "Light");
+        colorBox.getSelectionModel().select("Color");
+
+        materialBox.getItems().removeAll(colorBox.getItems());
+        materialBox.getItems().addAll("Cotton", "Denim", "Wool","Polyester");
+        materialBox.getSelectionModel().select("Material");
+    }
     @FXML
     private Button backButton;
     @FXML
@@ -14,6 +42,22 @@ public class CreateOrderController {
         HomePageApplication homePageApplication=new HomePageApplication();
         Stage window=(Stage)backButton.getScene().getWindow();
         homePageApplication.start(window);
+    }
+    @FXML
+    protected void onAddButtonClick() throws IOException {
+        HomePageApplication homePageApplication=new HomePageApplication();
+        Stage window=(Stage)addButton.getScene().getWindow();
+        homePageApplication.start(window);
+    }
+    @FXML
+    protected void onCheckOutButtonClick() throws IOException {
+        HomePageApplication homePageApplication=new HomePageApplication();
+        Stage window=(Stage)backButton.getScene().getWindow();
+        homePageApplication.start(window);
+    }
+    @FXML
+    protected void showTable() throws IOException {
+
     }
 
 }
