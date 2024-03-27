283e14d84f6b332ab61650e3e476da409c00451d
ChenYangNEU <yang.chen9@northeastern.edu>
2024-03-27 16:57:35 -0400

    Finished Bag

--- INFO6205FinalProject/src/main/java/com/info6205/algorithm/BagInterface.java
+++ INFO6205FinalProject/src/main/java/com/info6205/algorithm/BagInterface.java
@@ -1,4 +1,61 @@
 package com.info6205.algorithm;
 
-public interface BagInterface {
+public interface BagInterface<Order>{
+    /**
+     * Gets the current number of entries in this bag.
+     *
+     * @return The integer number of entries currently in the bag.
+     */
+    public int getCurrentSize();
+
+    /**
+     * Checks whether this bag is empty.
+     *
+     * @return True if the bag is empty, false otherwise.
+     */
+    public boolean isEmpty();
+
+    /**
+     * Adds a new entry to this bag.
+     *
+     * @param newEntry The object to be added as a new entry.
+     * @return True if the addition is successful, false if not.
+     */
+    public boolean add(Cloth newEntry);
+
+    /**
+     * Removes one unspecified entry from this bag, if possible.
+     *
+     * @return Either the removed entry, if the removal was successful, or null.
+     */
+    public Cloth remove();
+
+    /**
+     * Removes one occurrence of a given entry from this bag.
+     *
+     * @param anEntry The entry to be removed.
+     * @return True if the removal was successful, false if not.
+     */
+    public boolean remove(Cloth anEntry);
+
+    /**
+     * Removes all entries from this bag.
+     */
+    public void clear();
+
+    /**
+     * Counts the number of times a given entry appears in this bag.
+     *
+     * @param anEntry The entry to be counted.
+     * @return The number of times anEntry appears in the bag.
+     */
+    public int getFrequencyOf(Cloth anEntry);
+
+    /**
+     * Tests whether this bag contains a given entry.
+     *
+     * @param anEntry The entry to find.
+     * @return True if the bag contains anEntry, false if not.
+     */
+    public boolean contains(Cloth anEntry);
 }
--- INFO6205FinalProject/src/main/java/com/info6205/algorithm/Order.java
+++ INFO6205FinalProject/src/main/java/com/info6205/algorithm/Order.java
@@ -1,21 +1,30 @@
 package com.info6205.algorithm;
 
-public class Order {
+public class Order implements BagInterface<Order>{
     //this class forms as BAG!!!
+    //add cloths
     private int orderID;
     private Cloth[]cloths;
+    private int numberOfEntries;
     private String status;
     private double totalPrice;
     private PendingClothGroup[]groups;
 
+
+    public Order(){
+        this.orderID = 0;
+        this.cloths = new Cloth[25];
+        this.numberOfEntries = 0;
+        this.status = "unfinished";
+        this.totalPrice = 0.0;
+    }
     public Order(int orderID,Cloth[]cloths) {
         this.orderID=orderID;
         this.cloths=cloths;
         this.status="unfinished";
         this.totalPrice=0;
+        numberOfEntries = 0;
     }
-
-
 
     public int getOrderID() {
         return orderID;
@@ -55,5 +64,74 @@ public class Order {
 
     public void setGroups(PendingClothGroup[] groups) {
         this.groups = groups;
+    }
+
+    public boolean isArrayFull() {
+        return numberOfEntries >= cloths.length;
+    }
+    public boolean isEmpty(){
+        return numberOfEntries == 0;
+    }
+    public int getCurrentSize(){
+        return numberOfEntries;
+    }
+    public int getFrequencyOf(Cloth anEntry) {
+        int counter = 0;
+        for (int index = 0; index < numberOfEntries; index++) {
+            if (anEntry.equals(cloths[index])) {
+                counter++;
+            } // end if
+        } // end for
+        return counter;
+    }
+    public boolean contains(Cloth anEntry) {
+        return getIndexOf(anEntry) > -1;
+    }
+    public boolean add(Cloth newEntry) {
+        boolean result = true;
+        if (isArrayFull()) {
+            result = false;
+        } else { // Assertion: result is true here
+            cloths[numberOfEntries] = newEntry;
+            numberOfEntries++;
+        } // end if
+        return result;
+    }
+    private int getIndexOf(Cloth anEntry) {
+        int where = -1;
+        boolean stillLooking = true;
+        int index = 0;
+        while ( stillLooking && (index < numberOfEntries)) {
+            if (anEntry.equals(cloths[index])) {
+                stillLooking = false;
+                where = index;
+            } // end if
+            index++;
+        }
+        return where;
+    }
+    public void clear() {
+        while (!isEmpty()) {
+            remove();
+        }
+    }
+    public boolean remove(Cloth anEntry) {
+        int index = getIndexOf(anEntry);
+        Cloth result = removeEntry(index);
+        return anEntry.equals(result);
+    }
+    public Cloth remove() {
+        Cloth result = removeEntry(numberOfEntries - 1);
+        return result;
+    }
+    private Cloth removeEntry(int givenIndex) {
+        Cloth result = null;
+        if (!isEmpty() && (givenIndex >= 0)) {
+            result = cloths[givenIndex];
+            cloths[givenIndex] = cloths[numberOfEntries - 1];
+            cloths [numberOfEntries - 1] = null;
+            numberOfEntries--;
+        } // end if
+        return result;
     }
 }
