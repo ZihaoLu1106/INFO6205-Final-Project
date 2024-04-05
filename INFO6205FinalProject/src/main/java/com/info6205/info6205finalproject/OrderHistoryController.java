f805ed6334f79cb606013f0dd18710f247ab5636
HSU PEI-HAN <hsu.p@northeastern.edu>
2024-04-05 00:32:23 -0400

    add orderhistory

--- INFO6205FinalProject/src/main/java/com/info6205/info6205finalproject/OrderHistoryController.java
+++ INFO6205FinalProject/src/main/java/com/info6205/info6205finalproject/OrderHistoryController.java
@@ -1,12 +1,21 @@
 package com.info6205.info6205finalproject;
 
 import com.info6205.algorithm.Admin;
+import com.info6205.algorithm.Order;
+import com.info6205.algorithm.Cloth;
+import com.info6205.algorithm.PendingClothGroup;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,9 +26,41 @@ public class OrderHistoryController {
     private Scene scene;
     private Parent root;
     private Admin admin;
+    private ObservableList<PendingClothGroup> orderList;
+
     public void setAdmin(Admin admin) {
         this.admin = admin;
     }
+
+    @FXML
+    private TableView<PendingClothGroup> orderhistoryTable;
+    @FXML
+    private TableColumn<PendingClothGroup, String> orderIDColumn;
+    @FXML
+    private TableColumn<PendingClothGroup, String> clothNumColumn;
+    @FXML
+    private TableColumn<PendingClothGroup, String> methodColumn;
+
+
+
+    public void setOrderList(Admin admin) {
+        orderList = FXCollections.observableArrayList(admin.getQueue().getList());
+    }
+
+    @FXML
+    public void initialize() throws IOException {
+        showTable();
+    }
+    @FXML
+    protected void showTable() {
+
+        orderIDColumn.setCellValueFactory(data -> data.getValue().idProperty());
+        clothNumColumn.setCellValueFactory(data -> data.getValue().numProperty());
+        methodColumn.setCellValueFactory(data -> data.getValue().methodProperty());
+        orderhistoryTable.setItems(orderList);
+    }
+
+
     @FXML
     protected void onBackButtonClick(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("ManageOrder.fxml"));
